2b3beb14141a57c391da33e10582f1b5a39386b3
zlyaptica <maximov021@mail.ru>
2025-04-18 21:22:05 +0300

    replace security monitor file

--- example_2.py
+++ example_2.py
@@ -15,8 +15,8 @@ from src.example.my_security_monitor import MySecurityMonitor
 from src.system.security_policy_type import SecurityPolicy
 
 from src.system.config import CRITICALITY_STR, LOG_DEBUG, \
-    LOG_ERROR, LOG_INFO, DEFAULT_LOG_LEVEL, OPTICS_CONTROL_QUEUE_NAME, ORBIT_DRAWER_QUEUE_NAME
-
+    LOG_ERROR, LOG_INFO, DEFAULT_LOG_LEVEL, OPTICS_CONTROL_QUEUE_NAME, ORBIT_DRAWER_QUEUE_NAME, \
+    OPTICS_CONTROL_QUEUE_NAME, ORBIT_CONTROL_QUEUE_NAME, CAMERA_QUEUE_NAME, SECURITY_MONITOR_QUEUE_NAME
 
 def setup_system(queues_dir):
     # Симулятор спутника
